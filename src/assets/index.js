7e4f555d03e4d98ff045eab266b7dbb280b20b5d
sanspoof <alex.spencer@live.com>
2025-06-26 16:01:45 +0100

    Fix asset import case sensitivity for FF image

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ import estreamVideo from './estreamoverview.mp4';
 import superScaleVideo from './superscale.mp4';
 import easyImageCompress from './easyimagecompress.gif';
 import empireImg from './empire.webp';
-import ff from './ff.webp';
+import ff from './FF.webp';
 import piano from './piano.webp';
 import wwtbam from './wwtbam.webp';
 
