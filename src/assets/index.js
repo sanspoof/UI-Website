9c960ea7514b96f6a0c1802f5e03c60733d4df97
sanspoof <alex.spencer@live.com>
2025-06-27 11:22:46 +0100

    case change

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,6 +2,6 @@ export { default as estreamVideo } from './estreamoverview.mp4';
 export { default as superScaleVideo } from './superscale.mp4';
 export { default as easyImageCompress } from './easyimagecompress.gif';
 export { default as empireImg } from './empire.webp';
-export { default as ff } from './ff.webp';
+export { default as ff } from './FF.webp';
 export { default as piano } from './piano.webp';
 export { default as wwtbam } from './wwtbam.webp';
