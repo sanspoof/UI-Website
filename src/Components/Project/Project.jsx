e0079b2658bb76b7b355775f1603ad02e34656e2
sanspoof <alex.spencer@live.com>
2025-07-01 10:10:38 +0100

    layout tweak and added heroicons for later

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,3 +1,5 @@
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+
 function Project({ 
     name, 
     subtitle, 
@@ -18,12 +20,13 @@ function Project({
     return (
         <section className="grid lg:grid-cols-2 items-center gap-8 relative">
             {comingSoon && <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-100 text-black text-2xl font-bold">Coming Soon</div>}
-            <div className={`flex text-center items-center lg:align-baseline lg:items-baseline lg:text-left gap-4 lg:gap-3 flex-col justify-items-start ${comingSoon ? "opacity-15 pointer-events-none select-none" : ""}`}>
-                <h2 className="relative text-4xl font-black w-fit">{name}
-                    {beta && <span className="absolute -top-2 ml-2 text-lg rotate-6 bg-orange-600 text-white px px-2">Beta</span>}
-                    {InProgress && <span className="absolute -top-2 ml-2 text-lg rotate-6 bg-blue-600 text-white px px-2">In&nbsp;Progress</span>}
+            <div className={`flex text-center items-center lg:items-baseline lg:text-left gap-4 lg:gap-3 flex-col justify-items-start ${comingSoon ? "opacity-15 pointer-events-none select-none" : ""}`}>
+                <h2 className="flex flex-col items-center lg:items-baseline lg:block  relative text-4xl font-black w-fit">{name}
+                    {beta && <span className="w-fit lg:absolute lg:-top-2 lg:ml-2 text-lg lg:rotate-6 bg-orange-600 text-white px px-2">Beta</span>}
+                    {InProgress && <span className="w-fit lg:absolute lg:-top-2 lg:ml-2 text-lg lg:rotate-6 bg-blue-600 text-white px px-2">In&nbsp;Progress</span>}
                 </h2>
                 <h3 className="text-xl font-bold ">{subtitle}</h3>
+                {/* <ArrowRightIcon className="size-6 text-gray-500" /> */}
                 <p className="text-base text-gray-500 font-medium text-md lg:text-(length:--font-1125)">{description}</p>
                 <div className="flex flex-col lg:flex-row gap-1 items-center justify-center lg:justify-start">
                 {link && (
@@ -39,7 +42,7 @@ function Project({
             </div>
             
             <figure className={`flex justify-center lg:justify-end ${comingSoon ? "opacity-15 pointer-events-none select-none" : ""}` }>
-                {imgUrl && <img className="rounded-xl shadow-(--customShadow) max-w-(--projectmediamaxwidth) w-full" src={imgUrl} alt={name}  />}
+                {imgUrl && <img className="rounded-xl shadow-(--customShadow) max-w-(--projectmediamaxwidth) w-full" src={imgUrl} alt={name} loading="lazy"  />}
                 {videoURL && (
                     <video height="auto" className="rounded-xl shadow-(--customShadow) max-w-(--projectmediamaxwidth) w-full" autoPlay muted playsInline loop >
                         <source src={videoURL} type="video/mp4" />
