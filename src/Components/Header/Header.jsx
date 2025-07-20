a0640a81d38e450db3932b285864c1a45cc284ed
Alex Spencer <alex.spencer@live.com>
2025-07-20 13:28:07 +0100

    Reduce icon size in header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -10,10 +10,10 @@ function Header() {
             <a className="text-sm font-black" href="https://www.alexui.dev">Alex UI</a>
             <div className="flex gap-4 items-center">
                 <a title="GitHub" className="" href="https://github.com/sanspoof/" target="_blank" rel="noopener noreferrer">
-                    <img className="size-6" src={GitLogo} alt="GitHub Logo" />
+                    <img className="size-5" src={GitLogo} alt="GitHub Logo" />
                 </a>
                 <a title="LinkedIn" className="" href="https://www.linkedin.com/in/alex-spencer-7a298b46/" target="_blank" rel="noopener noreferrer">
-                <img className="size-6" src={LinkedIn} alt="LinkedIn Logo" />
+                <img className="size-5" src={LinkedIn} alt="LinkedIn Logo" />
                 </a>
             </div>
 
