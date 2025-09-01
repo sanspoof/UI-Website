fea080866f3beedf0e8979626eaffffe0246f6ad
sanspoof <alex.spencer@live.com>
2025-09-01 16:29:30 +0100

    Added Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Header from './Components/Header/Header';
 import Hero from './Components/Hero/Hero';
 import ProjectContainer from './Components/ProjectContainer/ProjectContainer';
 import { ThemeProvider } from './Context/ThemeContext';
+import { inject } from "@vercel/analytics"
+
+inject();
 
 function App() {
   return (
